04b997c00b4c32dadcd5c75ae7ea48efe926dda7
Nkwelle John <nkwellekwo@gmail.com>
2024-02-05 13:36:41 +0200

    Part 1 exercises 1.12-1.14

--- src/App.jsx
+++ src/App.jsx
@@ -1,133 +1,65 @@
 import { useState } from "react";
 
-function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [buttonId, setButtonId] = useState("");
-  const [allVotes, setAllVotes] = useState(0);
-  const [total, setTotal] = useState(0);
-
-  function handleFeedback(buttonId) {
-    setButtonId(buttonId);
-    setTotal((total) => total + 1);
-    if (buttonId === "good") {
-      setGood((good) => good + 1);
-      setAllVotes((votes) => votes + 1);
-    }
-    if (buttonId === "bad") {
-      setBad((bad) => bad + 1);
-      setAllVotes((votes) => votes - 1);
-    }
-    if (buttonId === "neutral") {
-      setNeutral((neutral) => neutral + 1);
-    }
-  }
-
-  const calAverageScore = function (allVotes, total) {
-    if (total === 0) return 0;
-    return allVotes / total;
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+  const maxVotes = Math.max(...votes);
+  const maxIndex = votes.indexOf(maxVotes);
+  const ranNumber = Math.floor(Math.random() * anecdotes.length);
+  console.log(votes);
+  const handleNext = function () {
+    setSelected(ranNumber);
+    console.log(selected);
   };
 
-  const calcPositiveFeedback = function (goodVotes, total) {
-    if (total === 0) return 0;
-    return (goodVotes / total) * 100;
+  const handleVote = function () {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
   };
-
-  let averageScore = +calAverageScore(allVotes, total).toFixed(1);
-  let positiveFeedback = +calcPositiveFeedback(good, total).toFixed(1);
 
   return (
     <>
-      <FeedBack handleFeedback={handleFeedback} />
-      <Statistic
-        good={good}
-        neutral={neutral}
-        bad={bad}
-        averageScore={averageScore}
-        positiveFeedback={positiveFeedback}
-        total={total}
-      />
+      <h2>Anecdotes of the day</h2>
+      <div>{anecdotes[selected]}</div>
+      <div>has {votes[selected]} votes</div>
+      <Button onHandleClick={handleVote} text="vote" />
+      <Button onHandleClick={handleNext} text="next anecdote" />
+      <Analysis message={anecdotes[maxIndex]} votes={maxVotes} />
     </>
   );
-}
+};
 
-function FeedBack({ handleFeedback }) {
-  return (
-    <div>
-      <h1>give feedback</h1>
-      <Button handleFeedback={handleFeedback} text="good" />
-      <Button handleFeedback={handleFeedback} text="neutral" />
-      <Button handleFeedback={handleFeedback} text="bad" />
-    </div>
-  );
-}
-
-function Button({ handleFeedback, text }) {
-  const handleClick = (e) => {
-    const buttonId = e.target.name;
-    handleFeedback(buttonId);
-  };
-
-  return (
-    <button name={text} onClick={handleClick}>
-      {text}
-    </button>
-  );
-}
-
-function Statistic({
-  good,
-  neutral,
-  bad,
-  averageScore,
-  positiveFeedback,
-  total,
-}) {
-  if (total === 0) {
+function Analysis({ votes, message }) {
+  if (votes === 0) {
     return (
       <>
-        <h2>Statistics</h2>
-        <p>No feedback given</p>
+        <h2>Anecdotes with the most votes</h2>
+        <div>No votes yet</div>
       </>
     );
   }
   return (
     <>
-      <h2>Statistics</h2>
-      <table>
-        <tbody>
-          <StatisticLine text="good" value={good} />
-          <StatisticLine text="neutral" value={neutral} />
-          <StatisticLine text="bad" value={bad} />
-          <StatisticLine text="all" value={total} />
-          <StatisticLine text="average" value={averageScore} />
-          <StatisticLine text="positive" value={positiveFeedback} />
-        </tbody>
-      </table>
+      <h2>Anecdotes with the most votes</h2>
+      <div>{message}</div>
+      <div>has {votes} votes</div>
     </>
   );
 }
 
-function StatisticLine({ text, value }) {
-  if (text === "positive") {
-    return (
-      <>
-        <tr>
-          <th>{text}</th>
-          <td>{value}%</td>
-        </tr>
-      </>
-    );
-  }
-  return (
-    <>
-      <tr>
-        <th>{text}</th>
-        <td>{value}</td>
-      </tr>
-    </>
-  );
+function Button({ onHandleClick, text }) {
+  return <button onClick={onHandleClick}>{text}</button>;
 }
 
 export default App;
